1fb2ff4694f8e90866b895ff286678a7baea8c46
mertoz41 <mertozkaynak@windowslive.com>
2020-05-13 14:48:47 -0400

    piglatin finished

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get "/" do
+        erb :user_input
+    end 
+
+    post "/piglatinize" do 
+        phrase = params[:user_phrase]
+        translate = PigLatinizer.new
+        @converted = translate.piglatinize(phrase)
+        erb :finished
+    end
+
+
+
+
+
 end
